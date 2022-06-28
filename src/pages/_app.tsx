def54a2de4084cf79cebc6dd15c74e3dfc469d36
Crazy slot <crazy.19.slot@gmail.com>
2022-06-28 16:51:08 +0100

    feat: 🎸 Update favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import type { FC } from 'react';
 import '~styles/globals.css';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => <div className="bg-slate-900 flex justify-center items-center h-screen"><Component {...pageProps} /></div>;
+const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => (
+	<>
+		<Head>
+			<title>Twenty five game</title>
+			<meta name="description" content="Twenty five game" />
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<div className="bg-slate-900 flex justify-center items-center h-screen">
+			<Component {...pageProps} />
+		</div>
+	</>
+);
 
 export default MyApp;
